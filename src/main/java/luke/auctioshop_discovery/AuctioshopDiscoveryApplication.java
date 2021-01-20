07503d9300a49c036f2e7e0feb77ea82513ade51
Lukas-max <lukaszjan88@gmail.com>
2021-01-20 16:05:40 +0100

    Initial commit

---
+++ src/main/java/luke/auctioshop_discovery/AuctioshopDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package luke.auctioshop_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AuctioshopDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctioshopDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/luke/auctioshop_discovery/AuctioshopDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package luke.auctioshop_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctioshopDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
